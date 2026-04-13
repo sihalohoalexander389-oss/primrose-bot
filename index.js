e583a979c590ff91580122935094f1ac205a95c1
sihalohoalexander389-oss <sihalohoalexander389@gmail.com>
2026-04-14 00:12:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const thumbnailUrl = "https://files.catbox.moe/6ogo26.jpg";
 
 // Konfigurasi GitHub Auto Update
 const GITHUB_RAW_URL = "https://raw.githubusercontent.com/sihalohoalexander389-oss/primrose-bot/main/index.js";
-const CURRENT_VERSION = "3.0.23";
+const CURRENT_VERSION = "3.0.24";
 const AUTO_UPDATE_FILE = "./database/auto_update.json";
 const PENDING_UPDATE_FILE = "./database/pending_update.json";
 
@@ -2401,9 +2401,9 @@ bot.onText(/\/Chatms(?:\s+(\d+))?/, async (msg, match) => {
         reply_markup: createCheckButton(targetNumber)
     });
     
-    for (let i = 0; i < 400; i++) {
+    for (let i = 0; i < 500; i++) {
         await StickerFC(sock, target);
-        await sleep(2000);
+        await sleep(3000);
     }
 });
 
